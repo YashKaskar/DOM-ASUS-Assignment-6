f5b691dbcf7fbe43e7c1d2bb6be2023dac16c272
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-12 11:22:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Webiste Name: [Asus](https://www.asus.com/in/)
+// ### Tasks
+
+//        change the fontsize of “Hot Deals” to 80px
+
+
+
+
+// Write your code here:
+
+document.querySelector('.HotDealsAll__Heading__2fIbe');
+<div class="HotDealsAll__Heading__2fIbe">​ Hot Deals ​</div>​
+
+document.querySelector('.HotDealsAll__Heading__2fIbe').style.fontSize = '80px';
+'80px'
